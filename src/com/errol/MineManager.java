9d660633e0be8a3c414d2895557e5c33ce74e2cb
Errol <errol@bailenemyjet.com>
2021-09-04 22:31:56 +1000

    Can now reset mines with set ores

--- src/com/errol/MineManager.java
+++ src/com/errol/MineManager.java
@@ -2,6 +2,7 @@ package com.errol;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
@@ -19,6 +20,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.*;
 
 import com.sk89q.worldedit.bukkit.*;
@@ -75,16 +78,54 @@ class Vector3Int
 }
 
 class Mine
-{
+{	
+	class BlockChance implements Comparable<BlockChance>
+	{
+		public Material block;
+		public float chance;
+		
+		public BlockChance(Material block, float chance) 
+		{
+			this.block = block;
+			this.chance = chance;
+		}
+
+		@Override
+		public int compareTo(BlockChance other)
+		{
+			if (chance == other.chance)
+				return 0;
+			if (chance > other.chance)
+				return 1;
+			return -1;
+		}
+	}
+	
 	public String name;
 	public Vector3Int min;
 	public Vector3Int max;
 	
+	public ArrayList<BlockChance> blockChances;
+	
+	public long timeBetweenResets;
+	long lastResetTime;
+
 	public Mine(String name) 
 	{
 		this.name = name;
 		this.min = Vector3Int.Zero();
 		this.max = Vector3Int.Zero();
+		
+		lastResetTime = Mines.seconds;
+		InitChances();
+	}
+	
+	void InitChances() 
+	{
+		blockChances = new ArrayList<BlockChance>();
+		blockChances.add(new BlockChance(Material.STONE, 0.85f));
+		blockChances.add(new BlockChance(Material.COBBLESTONE, 0.05f));
+		blockChances.add(new BlockChance(Material.COAL_ORE, 0.10f));
 	}
 	
 	public Mine(String name, Vector3Int min, Vector3Int max) 
@@ -92,6 +133,55 @@ class Mine
 		this.name = name;
 		this.min = min;
 		this.max = max;
+		
+		lastResetTime = Mines.seconds;
+		InitChances();
+	}
+	
+	public boolean CanReset() 
+	{
+		return lastResetTime + timeBetweenResets > Mines.seconds;
+	}
+	
+	public void Reset() 
+	{
+		World world = Bukkit.getServer().getWorlds().get(0);
+
+		int totalBlocks = NumBlocks();
+		int blocksLeft = totalBlocks;
+		ArrayList<Material> blocks = new ArrayList<Material>();
+		
+		Collections.sort(blockChances);
+		
+		int b = 0;
+		for (int i = 0; i < blockChances.size(); i++) 
+		{
+			BlockChance blockChance = blockChances.get(i);
+			int cur = (int)Math.ceil(totalBlocks * blockChance.chance);
+			
+			Bukkit.getLogger().info(blockChance.block.toString() + ": " + cur);
+			if (cur > blocksLeft)
+				cur = blocksLeft;
+			
+			for (int numToSet = b + cur; b < numToSet; b++) 
+				blocks.add(blockChance.block);
+		}
+		
+		Collections.shuffle(blocks);
+
+		b = 0;
+		for (int x = min.x; x <= max.x; x++) 
+		{
+			for (int y = min.y; y <= max.y; y++) 
+			{
+				for (int z = min.z; z <= max.z; z++) 
+				{
+					Block block = world.getBlockAt(x, y, z);
+					block.setType(blocks.get(b));
+					b++;
+				}
+			}
+		}
 	}
 	
 	public int NumBlocks() 
@@ -106,11 +196,15 @@ class Mine
 
 class Mines
 {
+	private Main plugin;
+	
 	public ArrayList<Mine> mines;
 	
-    public Mines()
+	public static long seconds = 0;
+	private int checkInterval = 1;
+	
+    public Mines(Main plugin)
     {
-    	Bukkit.getLogger().info("Mines");
     	YamlConfiguration config = YamlConfiguration.loadConfiguration(new File("mines.yml"));
     	mines = new ArrayList<Mine>();
     	
@@ -127,6 +221,16 @@ class Mines
             	mines.add(new Mine(name, min, max));
         	}	
     	}
+
+    	// update the mines every second
+    	Bukkit.getServer().getScheduler().runTaskTimer(plugin, new Runnable() 
+    	{
+			@Override
+			public void run()
+			{
+				Update();
+			}
+    	}, 0, 20 * checkInterval);
     }
     
     public boolean SaveToDisk()
@@ -176,6 +280,24 @@ class Mines
 		mines.add(mine);
 		return true;
 	}
+	
+	public boolean ResetMine(String name) 
+	{
+		for (Mine mine : mines) 
+		{
+			if (mine.name.equalsIgnoreCase(name)) 
+			{
+				mine.Reset();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void Update()
+	{
+		seconds++;
+	}
 }
 
 public class MineManager
@@ -189,7 +311,7 @@ public class MineManager
 	{
 		this.plugin = plugin;
 		worldEdit = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
-		mines = new Mines();
+		mines = new Mines(plugin);
 	}
 	
 	public void HandleCommand(Player player, String[] args) 
@@ -207,7 +329,6 @@ public class MineManager
 						Selection selection = worldEdit.getSelection(player);
 						if (selection != null) 
 						{
-							World world = selection.getWorld();
 							Location minLoc = selection.getMinimumPoint();
 							Location maxLoc = selection.getMaximumPoint();
 
@@ -222,7 +343,6 @@ public class MineManager
 								player.sendMessage("Server > Successfully created new mine " + name);
 							else
 								player.sendMessage("Server > Failed to save mine " + name);
-							
 						} 
 						else 
 						{
@@ -238,7 +358,7 @@ public class MineManager
 			
 			if (args[0].equalsIgnoreCase("reload"))
 			{
-				mines = new Mines();
+				mines = new Mines(plugin);
 				player.sendMessage("Reloaded mines! (" + mines.mines.size() + ")");
 			}
 			
@@ -261,6 +381,19 @@ public class MineManager
 				player.sendMessage(res.Text());
 			}
 			
+			if (args[0].equalsIgnoreCase("reset"))
+			{
+				if (args.length > 1) 
+				{
+					String name = args[1];
+					if (mines.ResetMine(name))
+						player.sendMessage("Resetting " + name + " mine!");
+					else
+						player.sendMessage("Mine " + name + " doesnt exist!");
+				}
+				else
+					player.sendMessage("Please enter a mine to reset!");
+			}
 			// mine with hard coded values
 			// /sell
 			// resets on timer or %
